a997a6d67b297dfdbf7959c4dd31dac5f5ac6f53
Ali Emir Şen <senaliemir@gmail.com>
2020-12-05 19:31:38 +0300

    add pause icon

--- src/components/startbutton/index.tsx
+++ src/components/startbutton/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { PlayIcon } from '../icons'
+import { PauseIcon, PlayIcon } from '../icons'
 
 export const StartButton = ({ active, ...props }) => {
   return (
@@ -8,7 +8,7 @@ export const StartButton = ({ active, ...props }) => {
       className='col-span-4 inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-lg font-bold text-white bg-purple-500 hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500'
       {...props}
     >
-      <PlayIcon className='mr-2' />
+      {active ? <PauseIcon className='mr-2' /> : <PlayIcon className='mr-2' />}
       {active ? 'Durdur' : 'Başlat'}
     </button>
   )
