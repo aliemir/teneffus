f6a47814ca6860fe9ae38202d58bdca1d8677f23
Ali Emir Şen <senaliemir@gmail.com>
2020-12-05 19:18:51 +0300

    update og meta

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,12 +7,34 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
+        <title>Ders Zamanlayıcı - teneffus.app</title>
         <meta
           name='description'
           content='Uzaktan eğitim dersleri için teneffüs zili uygulaması.'
         />
+
+        <meta property='og:url' content='https://teneffus.app' />
+        <meta property='og:type' content='website' />
+        <meta property='og:title' content='Ders Zamanlayıcı - teneffus.app' />
+        <meta
+          property='og:description'
+          content='Uzaktan eğitim dersleri için teneffüs zili uygulaması.'
+        />
         <meta
           property='og:image'
+          content='https://teneffus.app/social-share.png'
+        />
+
+        <meta name='twitter:card' content='summary_large_image' />
+        <meta property='twitter:domain' content='teneffus.app' />
+        <meta property='twitter:url' content='https://teneffus.app' />
+        <meta name='twitter:title' content='Ders Zamanlayıcı - teneffus.app' />
+        <meta
+          name='twitter:description'
+          content='Uzaktan eğitim dersleri için teneffüs zili uygulaması.'
+        />
+        <meta
+          name='twitter:image'
           content='https://teneffus.app/social-share.png'
         />
         <link rel='preconnect' href='https://fonts.gstatic.com' />
