6c0150d31bb8cbfc20ccf72b5a18806c5dde067b
Ali Emir Şen <senaliemir@gmail.com>
2020-12-05 18:30:15 +0300

    add meta desc

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,6 +7,10 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
+        <meta
+          name='description'
+          content='Uzaktan eğitim dersleri için teneffüs zili uygulaması.'
+        />
         <link rel='preconnect' href='https://fonts.gstatic.com' />
         <link
           href='https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700;800;900&display=swap'
