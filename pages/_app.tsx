bdd3c55d520ec0f0b37318b63f2333f7e5445c7b
Ali Emir Şen <senaliemir@gmail.com>
2020-12-05 18:18:11 +0300

    add font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,21 @@
 import { AppProps } from 'next/app'
+import Head from 'next/head'
 
 import '../styles/index.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link rel='preconnect' href='https://fonts.gstatic.com' />
+        <link
+          href='https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700;800;900&display=swap'
+          rel='stylesheet'
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
+
+export default MyApp
