456b34c9c7441050b79def8505b17ec234b49973
Ali Emir Şen <senaliemir@gmail.com>
2020-12-08 17:40:27 +0300

    update storage helper

--- src/definitions/index.ts
+++ src/definitions/index.ts
@@ -1,3 +1,4 @@
-export { getStoreDuration, setStoreDuration } from './storage'
+export { storage } from './storage'
 export { minutes, seconds } from './time'
 export { useInterval } from './useInterval'
+export { Colors } from './enums'
--- src/definitions/storage.ts
+++ src/definitions/storage.ts
@@ -1,20 +1,21 @@
-export const setStoreDuration = (minutes: number) => {
-  if (typeof window !== 'undefined' && window.localStorage) {
-    try {
-      localStorage.setItem('lecture-minutes', '' + minutes)
-    } catch (e) {
-      console.log(e)
+export const storage = {
+  set: (minutes: number) => {
+    if (typeof window !== 'undefined' && window.localStorage) {
+      try {
+        localStorage.setItem('lecture-minutes', '' + minutes)
+      } catch (e) {
+        console.log(e)
+      }
     }
-  }
-}
-
-export const getStoreDuration = () => {
-  if (typeof window !== 'undefined' && window.localStorage) {
-    const fromStorage = localStorage.getItem('lecture-minutes') ?? '30'
-    const parsed = parseInt(fromStorage, 10)
-    const duration = isNaN(parsed) ? 30 : parsed
-    return duration * 60
-  } else {
-    return 30 * 60
-  }
+  },
+  get: () => {
+    if (typeof window !== 'undefined' && window.localStorage) {
+      const fromStorage = localStorage.getItem('lecture-minutes') ?? '30'
+      const parsed = parseInt(fromStorage, 10)
+      const duration = isNaN(parsed) ? 30 : parsed
+      return duration * 60
+    } else {
+      return 30 * 60
+    }
+  },
 }
